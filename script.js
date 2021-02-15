31ad5c3caa9ab7ba3da1f7b69dc790f30b7dda80
Patti Perry <pattiperry12@gmail.com>
2021-02-15 10:44:06 -0500

    practiced variables

---
+++ script.js
@@ -0,0 +1,100 @@
+//alert("Hello, world")
+
+// console.log("howdy, world!!!!!!!!!!!!!!!!!!");
+
+// let myName = "Jordan"; // declaring and assigning
+
+// console.log(myName);
+
+// myName = "Penelope"; // reassigning
+
+// console.log(myName);
+
+// let myDogsName; // declaration
+
+// myDogsName = "Hoagie"; // assignment
+
+// let bioSentence = `My name is ${myName} and my dog's name is ${myDogsName}` // string interpolation
+// console.log(bioSentence)
+
+// // Numbers
+
+
+// let myDogsAge = 4;
+
+// let dogYearsPerYear = 7;
+
+// let myDogsAgeInHumanYears = myDogsAge*dogYearsPerYear;
+
+// let sum = "2" + "2"
+
+// // Boolean
+// let firstDayIsAlmostOver = true;
+
+
+
+// firstName should hold a string of your first name
+let firstName = "Jordan"
+// lastName should hold a string of your last name
+let lastName = "Castelloe"
+// age should hold a number of your age
+let age = 29
+// likesTravel should hold a boolean representing whether or not you like to travel
+let likesTravel = false
+// Log each variable to the console.
+console.log(firstName, lastName, age, likesTravel)
+
+
+let nameOne = "Vincent"
+
+let nameTwo = "Gabriel"
+
+let nameThree = "Aaron"
+
+let cohortFour = ["Vincent", "Gabriel", "Aaron", "Patti", "Mandy", 10000, true]
+console.log(cohortFour)
+
+console.log(cohortFour[2]) // this should be aaron
+
+console.log(cohortFour[4]) // this should be mandy
+
+console.log(cohortFour.length) // should be 7
+
+let favoriteMovies = ["Sandlot", "Matilda", "Ghost Busters"]
+
+console.log(favoriteMovies[0]) // should be sandlot
+
+console.log(favoriteMovies[1]) // should be matilda
+
+console.log(favoriteMovies[2]) // should be ghost busters
+
+
+// let sandlot = ["Sandlot", "Herbert Hoover", 1972]
+
+let sandlot = {
+    title: "Sandlot", // key value pair
+    director: {
+        name: "Herbert Hoover",
+        yearBorn: 1973
+    },
+    yearMade: 1993,
+    cast: ["Tom Geary", "Mike Vicauoeirueoirualdsfjoeiwrueoiru", "Davis Leary"],
+}
+
+console.log(sandlot)
+
+console.log(sandlot.title) // should be "sandlot"
+
+// sandlot.director = "David Mickey Evans"
+
+console.log(sandlot.cast[2])
+
+console.log(sandlot.director.yearBorn)
+
+let jordanObject = {
+   firstName: "Jordan",
+   lastName: "Castelloe",
+   age: 29,
+   likesToTravel: false,
+   favoriteMovies: favoriteMovies
+}
